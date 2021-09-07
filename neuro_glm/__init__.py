0241c14938396566c441325996e04028842859e4
Arne Meyer <arne.f.meyer@gmail.com>
2021-09-07 23:42:24 +0200

    fix import

--- neuro_glm/__init__.py
+++ neuro_glm/__init__.py
@@ -1,1 +1,2 @@
 
+from ._glm import PoissonGLM
--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Author: Arne F. Meyer <arne.f.meyer@gmail.com>
+# License: GPLv3
 
 from setuptools import setup, find_packages
 
